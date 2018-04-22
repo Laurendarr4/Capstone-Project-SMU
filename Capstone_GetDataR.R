e32726928e6626a16a91471d7620c5a0fdccba81
Mike Crowder <crowder.mike@gmail.com>
2018-04-22 10:45:02 -0500

    Added lat/lon cleaning code start inDev
    Still need to separate lat from lon and remove quoates. Then these need to be merged back into data set.

--- Capstone_GetDataR.R
+++ Capstone_GetDataR.R
@@ -1,3 +1,4 @@
+
 ###Capstone Project
 ##Dallas Open Data-Police Incidents
 #3/31/2018
@@ -7,6 +8,9 @@
 ## install.packages("RSocrata")
 
 library("RSocrata")
+library(tidyr)
+library(stringr)
+library(readr)
 
 #Use soDA URL to access Incident Data; set limit on number of rows per page to 500 for now
 url= "https://www.dallasopendata.com/resource/qqc2-eivj.csv?$limit=500&$offset=0"
@@ -16,7 +20,18 @@ df <- read.socrata(url, app_token = NULL, email= NULL, password= NULL)
 #Check for correct data loaded
 head(df)
 
-#select rows of importance
+# Take the Point and () out of the location1 data
+df$latlon <- gsub(".*\\((.*)\\).*", "\\1", df$location1)
+# Check to see if Point and () are removed
+head(df$latlon)
+
+#Seperates into list
+latLongList <- str_split_fixed(df$latlon, " ", 2)
+head(latLongList)
+
+
+
+
 
 
 
