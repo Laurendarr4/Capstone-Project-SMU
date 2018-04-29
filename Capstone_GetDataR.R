fe1b06bbe22de28e26bf202a812c38d3aba8bf96
Mike Crowder <crowder.mike@gmail.com>
2018-04-29 07:23:25 -0500

    Lat/Lng cleaning continued
    Still no method to get the Lat/Lng data back into the data frame to map.

--- Capstone_GetDataR.R
+++ Capstone_GetDataR.R
@@ -21,20 +21,33 @@ df <- read.socrata(url, app_token = NULL, email= NULL, password= NULL)
 head(df)
 
 # Take the Point and () out of the location1 data
-df$latlon <- gsub(".*\\((.*)\\).*", "\\1", df$location1)
-# Check to see if Point and () are removed
-head(df$latlon)
+# Seperate Point and ()
+df$latlon <- gsub(".*\\((.*)\\).*","\\1", df$location1)
+# Spilt out into list 2 columns
+LatLong <- str_split_fixed((df$latlon)," ",2)
+# Name columns in list
+colnames(LatLong) <- c("Latitue", "Longitude")
+# Covert into dataframe
+df_LatLong <-as.data.frame(LatLong)
+# Bring into current frame
+# Convert to Numeric
+df$Latitude <- as.numeric(df_LatLong$Latitue)
+df$Longitude <- as.numeric(df_LatLong$Longitude)
 
-#Seperates into list
-latLongList <- str_split_fixed(df$latlon, " ", 2)
-head(latLongList)
-
-
-
-
-
+# Does not show up, needs to be merged some how or another method developed
+View(df)
 
 
 ###Install packages for mapping
-install.packages("leaflet")
+#install.packages("leaflet")
 library("leaflet")
+
+d <- leaflet() %>% setView(lng = -96.7970, lat = 32.7767, zoom = 10)
+d %>% addProviderTiles(providers$Stamen.Toner) %>%
+  addCircleMarkers(lng = df$Longitude,
+    lat = df$Latitude,
+    popup = df$`Type of Location`,
+    radius = 8,
+    stroke = FALSE,
+    fillOpacity = 0.75,
+    color = "red")
