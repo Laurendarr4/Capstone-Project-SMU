b5086e557e38e90762af39f243093c7419ca6abc
Mike Crowder <crowder.mike@gmail.com>
2018-06-17 10:05:30 -0500

    Add files via upload

--- dotMapCorrelation.R
+++ dotMapCorrelation.R
@@ -3,6 +3,16 @@
 ### This will bring in both Commercial and Residential
 ### burglary along with Criminal Mischief/Vandalism
 
+## Install the required package
+
+library(RSocrata)
+library(tidyr)
+library(stringr)
+library(readr)
+library(dplyr)
+library(leaflet)
+library(leaflet.extras)
+
 url2 = "https://www.dallasopendata.com/resource/qqc2-eivj.csv?$where=ucroffense like '%BURGLARY%' OR ucroffense like '%VANDALISM%'"
 
 df2 <- read.socrata(url2, app_token = NULL, email= NULL, password = NULL)
@@ -30,3 +40,71 @@ rm(df_LatLon)
 
 # Check UCR Offense
 table(df2$ucroffense)
+
+# Remove obs w/o location
+# df2 <- df2[!(is.na(df2$location1) | df2$location1==""),]
+
+# Build year frames
+df2_2015 <- df2[ which(df2$year1=='2015'),]
+df2_2016 <- df2[ which(df2$year1=='2016'),]
+df2_2017 <- df2[ which(df2$year1=='2017'),]
+
+# Create a palette
+pal <- colorFactor(c("#A0A0FF", "#FFA0A0", "#4EED34"), 
+          domain = c("BURGLARY-BUSINESS",
+                    "BURGLARY-RESIDENCE",
+                    "VANDALISM & CRIM MISCHIEF"))
+
+# 2015 Map
+bCM_2015 <- leaflet() %>% setView(lng = -96.7970, lat = 32.7767, zoom = 11)
+bCM_2015 %>% addProviderTiles(providers$Stamen.Toner) %>%
+  addCircleMarkers(lng = df2_2015$Longitude,
+    lat = df2_2015$Latitude,
+    popup = paste("MO: ", df2_2015$mo, "<br>",
+      "Premise: ", df2_2015$premise, "<br>",
+      "Date: ", df2_2015$date1, "<br>",
+      "Day: ", df2_2015$day1,
+      "Time: ", df2_2015$time1,
+      "Zip: ", df2_2015$zipcode),
+    radius = 5,
+    stroke = FALSE,
+    fillOpacity = 0.75,
+    color = pal(df2_2015$ucroffense))%>%
+  addLegend("bottomright", pal = pal, values = df2_2015$ucroffense,
+    title = "2015 Dallas Burg ~ V&CM")
+
+# 2016 Map
+bCM_2016 <- leaflet() %>% setView(lng = -96.7970, lat = 32.7767, zoom = 11)
+bCM_2016 %>% addProviderTiles(providers$Stamen.Toner) %>%
+  addCircleMarkers(lng = df2_2016$Longitude,
+    lat = df2_2016$Latitude,
+    popup = paste("MO: ", df2_2016$mo, "<br>",
+      "Premise: ", df2_2016$premise, "<br>",
+      "Date: ", df2_2016$date1, "<br>",
+      "Day: ", df2_2016$day1,
+      "Time: ", df2_2016$time1,
+      "Zip: ", df2_2016$zipcode),
+    radius = 5,
+    stroke = FALSE,
+    fillOpacity = 0.75,
+    color = pal(df2_2016$ucroffense))%>%
+  addLegend("bottomright", pal = pal, values = df2_2016$ucroffense,
+    title = "2016 Dallas Burg ~ V&CM")
+
+# 2017 Map
+bCM_2017 <- leaflet() %>% setView(lng = -96.7970, lat = 32.7767, zoom = 11)
+bCM_2017 %>% addProviderTiles(providers$Stamen.Toner) %>%
+  addCircleMarkers(lng = df2_2017$Longitude,
+    lat = df2_2017$Latitude,
+    popup = paste("MO: ", df2_2017$mo, "<br>",
+      "Premise: ", df2_2017$premise, "<br>",
+      "Date: ", df2_2017$date1, "<br>",
+      "Day: ", df2_2017$day1,
+      "Time: ", df2_2017$time1,
+      "Zip: ", df2_2017$zipcode),
+    radius = 5,
+    stroke = FALSE,
+    fillOpacity = 0.75,
+    color = pal(df2_2017$ucroffense))%>%
+  addLegend("bottomright", pal = pal, values = df2_2017$ucroffense,
+    title = "2017 Dallas Burg ~ V&CM")
